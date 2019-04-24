a39bc8cdae9c5c6c6eb8586ad78abfa464017b6d
Vasile Boris <boris@espressoprogrammer.com>
2019-04-24 07:19:16 +0300

    Add screens folder and move BookScreenComponent there

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import {
 import { Provider } from 'react-redux';
 import { library }  from 'reducers/LibraryReducer';
 import localizer from 'utils/Localizer';
-import BookPageComponent from 'components/book/BookPageComponent';
+import BookScreenComponent from 'components/screens/BookScreenComponent';
 import appStyles from 'styles/AppStyles';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -33,7 +33,7 @@ class App extends React.Component {
         return isLocalizerInitialized && (
             <View style={[appStyles.app, appStyles.container, appStyles.vertical, appStyles.justifyStart]}>
                 <Provider store={store}>
-                    <BookPageComponent bookUuid={'1e4014b1-a551-4310-9f30-590c3140b695'}/>
+                    <BookScreenComponent bookUuid={'1e4014b1-a551-4310-9f30-590c3140b695'}/>
                 </Provider>
             </View>
         );
