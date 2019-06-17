5ac5ebf1674b6e6c16d53b2b17897c3e5fb1b48b
Vasile Boris <boris@espressoprogrammer.com>
2019-06-17 13:28:13 +0300

    Update about screen

--- assets/about/AboutContent.js
+++ assets/about/AboutContent.js
@@ -1,7 +1,49 @@
 export const content = `
-Markdown in react-native is so cool!
+# Application details
 
-You can **emphasize** what you want, or just _suggest it_ 😏…
+## Author
 
-You can even [**link your website**](https://twitter.com/Charles_Mangwa) or if you prefer: [email somebody](mailto:email@somebody.com)
+[Vasile Boris][about-me]
+
+## Application details
+
+* Version: 1.0.0
+* License: [MIT][license-mit]
+
+## Application details
+
+* [React Native][react-native]
+* [React Navigation][react-navigation]
+* [React][react]
+* [Redux][redux]
+* [Redux-Saga][redux-saga]
+
+## Credits
+
+### book, open-book icons
+
+* Author: [Icons Field][school-education-32-author]
+* License: [Attribution 3.0 Unported (CC BY 3.0)][creativecommons-unported-3.0] 
+* Source: [School & Education, Iconfinder][school-education-32-source]
+* Changes: Updated format from svg to png and changed color
+
+### menu, home, help, info icons
+
+* Author: [Stephen Hutchings][typicons-2-author]
+* License: [Attribution 3.0 Unported (CC BY 3.0)][creativecommons-unported-3.0] 
+* Source: [School & Education, Iconfinder][typicons-2-source]
+* Changes: Updated format from svg to png and changed color
+
+[about-me]: https://espressoprogrammer.com/about-me/
+[license-mit]: https://opensource.org/licenses/MIT
+[react-native]: https://facebook.github.io/react-native/
+[react-navigation]: https://reactnavigation.org/en/
+[react]: https://reactjs.org/
+[redux]: https://redux.js.org/
+[redux-saga]: https://redux-saga.js.org/
+[school-education-32-source]: https://www.iconfinder.com/iconsets/school-education-32
+[school-education-32-author]: https://www.iconfinder.com/dreamview
+[typicons-2-source]: https://www.iconfinder.com/iconsets/typicons-2
+[typicons-2-author]: https://www.s-ings.com/me/
+[creativecommons-unported-3.0]: https://creativecommons.org/licenses/by/3.0/
 `;
