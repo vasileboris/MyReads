12fa40bb0f771aca3f234905ad856b12aa2d446f
Vasile Boris <boris@espressoprogrammer.com>
2022-01-06 09:05:50 +0200

    Use lastReadPageDate with precedence over lastReadPageDate on sorting books

--- components/book/BooksComponent.js
+++ components/book/BooksComponent.js
@@ -6,14 +6,18 @@ import {
 import BookComponent from './BookComponent';
 import Card from '/components/card/Card';
 import appStyles from 'styles/AppStyles';
+import { getISODate } from 'utils/Date';
 
 function BooksComponent(props) {
     const { books, onBookClick } = props;
     return (
         <FlatList style={[appStyles.resultDetail]}
             data={Object.values(books).sort((book1, book2) => {
-                if(book1.updateDate && book2.updateDate && book1.updateDate !== book2.updateDate) {
-                    return book2.updateDate.localeCompare(book1.updateDate);
+                const book1Date = getBookDate(book1);
+                const book2Date = getBookDate(book2);
+
+                if(book1Date !== book2Date) {
+                    return book2Date.localeCompare(book1Date);
                 }
 
                 return book1.title.toLowerCase().localeCompare(book2.title.toLowerCase());
@@ -26,6 +30,11 @@ function BooksComponent(props) {
             )}
         />
     );
+}
+
+function getBookDate(book) {
+    const bookDate = book.lastReadPageDate ? book.lastReadPageDate : book.lastReadPageDate;
+    return bookDate ? bookDate :  getISODate(new Date());
 }
 
 BooksComponent.propTypes = {
