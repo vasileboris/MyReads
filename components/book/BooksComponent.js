4bfc5f84deb6f870e089caab6dce0dd913380cea
Vasile Boris <boris@espressoprogrammer.com>
2022-01-07 08:23:08 +0200

    Sort unread books first

--- components/book/BooksComponent.js
+++ components/book/BooksComponent.js
@@ -13,13 +13,20 @@ function BooksComponent(props) {
     return (
         <FlatList style={[appStyles.resultDetail]}
             data={Object.values(books).sort((book1, book2) => {
+                // Unread books first
+                if((book1.readPercentage === 100 && book2.readPercentage < 100)
+                    || (book1.readPercentage < 100 && book2.readPercentage === 100)) {
+                    return book1.readPercentage - book2.readPercentage;
+                }
+
+                // and then sort by date desc
                 const book1Date = getBookDate(book1);
                 const book2Date = getBookDate(book2);
-
                 if(book1Date !== book2Date) {
                     return book2Date.localeCompare(book1Date);
                 }
 
+                // and then by title asc
                 return book1.title.toLowerCase().localeCompare(book2.title.toLowerCase());
             })}
             keyExtractor={book => book.uuid}
