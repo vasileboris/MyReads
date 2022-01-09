6d94dcf3d1ea85429057a6fb9dd4f862cd8c1369
Vasile Boris <boris@espressoprogrammer.com>
2022-01-09 12:50:38 +0200

    Update version

--- assets/about/AboutContent.js
+++ assets/about/AboutContent.js
@@ -4,7 +4,7 @@ export const content = `
 ## Application details
 
 * Author: [Espresso Programmer][about-me]
-* Version: 1.0.7
+* Version: 1.1.2
 * License: [MIT][license-mit]
 
 ## Built with Open Source Software
